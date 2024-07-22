385c8fc0790b8cbba06b2addc76bdfc78290fdb9
William Takeshi <williamtpereira@gmail.com>
2024-07-22 10:58:42 -0300

    re-structure project

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,26 @@
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpServer};
 
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route(
+                "/health_check",
+                web::get().to(routes::health_check::health_check),
+            )
+            .route(
+                "/subscriptions",
+                web::post().to(routes::subscriptions::subscribe),
+            )
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
