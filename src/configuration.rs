d985d7fd0aa99fe06aa2f392d09f79b2a5003d9f
William Takeshi <williamtpereira@gmail.com>
2024-07-22 13:13:34 -0300

    fix ci jobs

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,1 +1,31 @@
+use config::{Config, File, FileFormat};
 
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
+
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    Config::builder()
+        .add_source(File::new("configuration", FileFormat::Yaml))
+        .build()?
+        .try_deserialize()
+}
