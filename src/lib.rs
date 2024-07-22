627a87d83ddbdaf569485ef586169e33e24ca26c
William Takeshi <williamtpereira@gmail.com>
2024-07-22 15:34:20 -0300

    fix tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,15 @@ use std::net::TcpListener;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 
 pub mod configuration;
 pub mod routes;
 pub mod startup;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route(
                 "/health_check",
@@ -18,6 +20,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
                 "/subscriptions",
                 web::post().to(routes::subscriptions::subscribe),
             )
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
